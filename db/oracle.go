da243da5f35494ad5d9f73b966fbba860fa404db
Jayder França <jayderfranca@gmail.com>
2017-04-05 00:15:41 -0300

    Arquivo de definicoes renomeado

--- db/oracle.go
+++ db/oracle.go
@@ -1,7 +1,11 @@
 package db
 
+import (
+	"github.com/jayderfranca/dbsamples/defs"
+)
+
 type Oracle struct {
-	conn Connection
+	conn *defs.Connection
 }
 
 func (db *Oracle) ValidateString(string string) error {
